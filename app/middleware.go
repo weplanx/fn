d2d191332fed24f1dc9cf7c5d301377b42776c61
kain <zhangtqx@vip.qq.com>
2022-04-15 09:06:11 +0800

    更新

--- app/middleware.go
+++ app/middleware.go
@@ -1,17 +1,25 @@
 package app
 
 import (
+	"github.com/gin-contrib/requestid"
 	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
 	"github.com/weplanx/openapi/common"
 	"go.uber.org/zap"
+	"os"
 	"time"
 )
 
 func globalMiddleware(r *gin.Engine, values *common.Values) *gin.Engine {
+
+	if os.Getenv("GIN_MODE") == "release" {
+		logger, _ := zap.NewProduction()
+		r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
+		r.Use(requestid.New())
+	} else {
+		r.Use(gin.Logger())
+	}
 	r.SetTrustedProxies(values.TrustedProxies)
-	logger, _ := zap.NewProduction()
-	r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
 	r.Use(gin.CustomRecovery(catchError))
 	return r
 }
--- go.mod
+++ go.mod
@@ -4,12 +4,13 @@ go 1.15
 
 require (
 	github.com/caarlos0/env/v6 v6.9.1
+	github.com/gin-contrib/requestid v0.0.3 // indirect
 	github.com/gin-contrib/zap v0.0.2
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/google/wire v0.5.0
 	github.com/json-iterator/go v1.1.11
-	github.com/weplanx/go v0.0.1
+	github.com/weplanx/go v0.0.2
 	go.mongodb.org/mongo-driver v1.8.0
 	go.uber.org/zap v1.19.1
 )
