1478edb473b162982401a9ad3e1a3ab13795ca47
kain <zhangtqx@vip.qq.com>
2023-01-26 13:40:54 +0800

    UPDATE: Excel Function

--- functions/excel/api/api.go
+++ functions/excel/api/api.go
@@ -1,10 +1,16 @@
 package api
 
 import (
+	"bytes"
+	"context"
 	"excel/common"
 	"fmt"
 	"github.com/bytedance/sonic/decoder"
+	"github.com/vmihailenco/msgpack/v5"
+	"github.com/xuri/excelize/v2"
+	"io"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -14,7 +20,7 @@ type API struct {
 
 type M map[string]interface{}
 
-type Data struct {
+type Result struct {
 	Records []Record `json:"records"`
 }
 
@@ -59,17 +65,81 @@ func (x *API) EventInvoke(w http.ResponseWriter, req *http.Request) {
 	if req.Method != "POST" {
 		return
 	}
+	ctx := req.Context()
 
-	var data Data
-	if err := decoder.NewStreamDecoder(req.Body).Decode(&data); err != nil {
+	var result Result
+	if err := decoder.
+		NewStreamDecoder(req.Body).
+		Decode(&result); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	for _, record := range data.Records {
-		fmt.Println(record.Cos.Url)
+	for _, record := range result.Records {
+		key := strings.Replace(record.Cos.Url, x.Values.Cos.Url, "", -1)
+		resp, err := x.Client.Object.Get(ctx, key, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if err = x.toExcel(ctx, resp.Body); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf(`已触发: %s`, time.Now())))
+}
+
+type Metadata struct {
+	Name  string   `msgpack:"name"`
+	Parts []string `msgpack:"parts"`
+}
+
+func (x *API) toExcel(ctx context.Context, body io.Reader) (err error) {
+	var metadata Metadata
+	if err = msgpack.NewDecoder(body).Decode(&metadata); err != nil {
+		return
+	}
+	file := excelize.NewFile()
+	defer file.Close()
+	for _, key := range metadata.Parts {
+		var streamWriter *excelize.StreamWriter
+		if streamWriter, err = file.NewStreamWriter(metadata.Name); err != nil {
+			return
+		}
+		resp, err := x.Client.Object.Get(ctx, key, nil)
+		if err != nil {
+			return
+		}
+		dec := msgpack.NewDecoder(resp.Body)
+		rowID := 1
+		for {
+			var data []interface{}
+			if err := dec.Decode(&data); err != nil {
+				if err == io.EOF {
+					break
+				}
+				return
+			}
+			cell, _ := excelize.CoordinatesToCellName(1, rowID)
+			if err = streamWriter.SetRow(cell, data); err != nil {
+				return
+			}
+			rowID++
+		}
+		if err = streamWriter.Flush(); err != nil {
+			return
+		}
+	}
+	var buf *bytes.Buffer
+	if buf, err = file.WriteToBuffer(); err != nil {
+		return
+	}
+	key := fmt.Sprintf(`%s.xlsx`, metadata.Name)
+	if _, err = x.Client.Object.Put(ctx, key, buf, nil); err != nil {
+		return
+	}
+	return
 }
